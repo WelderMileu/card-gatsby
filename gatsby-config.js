8e2c08d9f3faa622e85bca2101b4d67a776ebc88
WelderMileu <welderfelix21@gmail.com>
2020-03-12 17:28:31 -0300

    alterando o arquivo de configuracao do gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	siteMetadata : {
 		title:`Menininha`,
+		siteUrl: `https://card-gatsby.netlify.com/`,
+		description: `Menina em um card construido com tailwindcss`,
 	},
 
 	plugins : [

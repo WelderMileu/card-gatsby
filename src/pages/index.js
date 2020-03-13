38182fd592783b130ed4bd305b592232150e43b4
WelderMileu <welderfelix21@gmail.com>
2020-03-13 18:30:06 -0300

    adionando seo no site

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import Container from './components/container';
 import Card from './components/card';
 
+import { GatsbySeo } from 'gatsby-plugin-next-seo';
+
 function App(){
 	return(
 		<Container>
+			<GatsbySeo 
+				title="Site da menina"
+				description="There are many variations of passages of Lorem Ipsum available, 
+					  but the majority have suffered alteration in some form."
+			/>
+			
 			<Card
 				title="Julinha"
 				text="There are many variations of passages of Lorem Ipsum available, 
